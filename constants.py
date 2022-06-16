ca09cbda4051595b1f1fb37bea12fc2aabadef6a
Selman N. Tabet <33247400+selmantabet@users.noreply.github.com>
2022-06-16 17:35:12 +0300

    Cleared out some constants.

--- constants.py
+++ constants.py
@@ -11,13 +11,9 @@ It contains all the keys and strings used to test out different parts of the pro
 """
 
 POSTGRESQL_DB_NAME = "falconflex-integration"
-POSTGRESQL_AZURE_DB_NAME = "postgres"
-POSTGRESQL_PORT = 5432
 POSTGRESQL_ADDRESS = "localhost"
-POSTGRESQL_HOST_AZURE = "integration-gateway-postgres.postgres.database.azure.com"
 POSTGRESQL_USER = "postgres"
 POSTGRESQL_PASSWORD = "Selman@123"
-POSTGRESQL_AZURE_PASSWORD = "Selman_123"
 
 API_KEY = "0d1520ca4fb7f27f1782fe0b7c0451bc"
 API_SECRET = "6955f2ab1a939dd4689e0633fa934a0a"
@@ -32,19 +28,7 @@ MERCHANT_TOKEN_OLD = "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"
 
 FLEET_MANAGEMENT_HOST = "integrationgateway-staging-lb.falconflex.ai"
 FLEET_MANAGEMENT_URI = "https://integrationgateway-staging-lb.falconflex.ai"
-FLEET_MANAGEMENT_URI_TEST = "https://webhook.site/3cc94e74-27fb-4bfa-b9e8-350bc6d5a206"
 FLEET_AUTH_TOKEN = "15D7E1D1F22D3F415C346E0DB3D2D6A50912F90461C355221F455B5DA4E24A43"
-
-INTEGRATION_GATEWAY = "https://integration-gateway.azurewebsites.net"
-INTEGRATION_GATEWAY_HOST = "webhook.site"
-INTEGRATION_GATEWAY_TEST_NOSCHEME = "webhook.site/d18990f9-038f-4c9f-b20b-6ead5f45bb3f"
-INTEGRATION_GATEWAY_TEST = "https://webhook.site/44a25e69-f3ce-4b30-81bd-f3210e4b230e"
-INTEGRATION_GATEWAY_DUMMY = "https://webhook.site/testhook"
-INTEGRATION_GATEWAY_TASK_CREATE = "https://webhook.site/d18990f9-038f-4c9f-b20b-6ead5f45bb3f/tasks"
-INTEGRATION_GATEWAY_TASK_UPDATES = "https://webhook.site/d18990f9-038f-4c9f-b20b-6ead5f45bb3f/taskscallback"
-
-INTEGRATION_GATEWAY_FROM_SHOP = "https://webhook.site/d18990f9-038f-4c9f-b20b-6ead5f45bb3f?company_id=620ac4fe812b6a4a8977e007&fleet_token=15D7E1D1F22D3F415C346E0DB3D2D6A50912F90461C355221F455B5DA4E24A43"
-INTEGRATION_GATEWAY_FROM_FLEET = "https://webhook.site/d18990f9-038f-4c9f-b20b-6ead5f45bb3f?merchant=falconflex-test&token=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"
 
 COMPANY_ID_TEST = "620ac4fe812b6a4a8977e007"
 
--- constants_prod.py
+++ constants_prod.py
@@ -7,6 +7,8 @@ This file contains constants that may be altered to fit business needs and is
 meant to be used in production.
 """
 
+INTEGRATION_GATEWAY = "https://integration-gateway.azurewebsites.net"
+
 # Time (in minutes) between webhook generation to pickupTime
 TIME_BUFFER_MINUTES = 5
 
@@ -15,10 +17,3 @@ SHOPIFY_API_VERSION = '2022-04'
 INTEGRATION_GATEWAY_PROD = "https://integration-gateway.azurewebsites.net"
 FLEET_AUTH_TOKEN_PROD = "15D7E1D1F22D3F415C346E0DB3D2D6A50912F90461C355221F455B5DA4E24A43"
 FLEET_MANAGEMENT_URI_PROD = "https://integrationgateway-staging-lb.falconflex.ai"
-
-# POSTGRESQL_DB_NAME = "falconflex-integration"
-# POSTGRESQL_AZURE_DB_NAME = "postgres"
-# POSTGRESQL_PORT = 5432
-# POSTGRESQL_HOST_AZURE = "integration-gateway-postgres.postgres.database.azure.com"
-# POSTGRESQL_USER = "postgres"
-# POSTGRESQL_AZURE_PASSWORD = "Selman_123"
