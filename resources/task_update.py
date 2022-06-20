683f86339a08f7a9fb529581c0f8307dfc31acda
Selman N. Tabet <33247400+selmantabet@users.noreply.github.com>
2022-06-20 15:51:59 +0300

    Minor fix.

--- resources/task_update.py
+++ resources/task_update.py
@@ -19,10 +19,10 @@ class Task_Update(Resource):
             return {"errors": "Task Update Payload JSON could not be decoded."}, 400
         except:
             return {"errors": "Something went wrong with parsing incoming data."}, 400
+        task_update_payload = task_update_payload_raw["Data"]  # Cleaned
         if verbose:
             print("Incoming raw payload: ", task_update_payload_raw)
             print("Incoming cleaned payload: ", task_update_payload)
-        task_update_payload = task_update_payload_raw["Data"]  # Cleaned
         task_update_metafields = task_update_payload["MetaDataFields"]
         order_id = task_update_payload["ClientGeneratedId"]
         if "merchant_url" not in task_update_metafields:
