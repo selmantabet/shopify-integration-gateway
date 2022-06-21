c5d93b66d9186db3ffb380ef5d0cb2f81ca23a19
Selman N. Tabet <33247400+selmantabet@users.noreply.github.com>
2022-06-21 15:47:46 +0300

    Setup script overhaul

--- setup/setup_fleet.py
+++ setup/setup_fleet.py
@@ -1,6 +1,16 @@
-from constants_prod import *
-from callback_functions import *
-fleet_callback_created = subscribe_fleet(FLEET_MANAGEMENT_URI_PROD,
-                                         FLEET_AUTH_TOKEN_PROD, INTEGRATION_GATEWAY)
+import os
+from dotenv import load_dotenv
+
+from callback_functions import subscribe_fleet
+
+cwd = os.getcwd()
+env_path = os.path.join(cwd, "env", "vars.env")
+print("cfg env path: ", env_path)
+if (os.path.exists(env_path)):
+    load_dotenv(dotenv_path=env_path)
+    print("env vars initialized from setup script")
+
+fleet_callback_created = subscribe_fleet(os.environ["FLEET_MANAGEMENT_URI"],
+                                         os.environ["FLEET_AUTH_TOKEN"], os.environ["INTEGRATION_GATEWAY"])
 
 print(fleet_callback_created)
